02b79071003792f3c3c58ba093066266d99e3eba
Izxyx <arq_h@outlook.com>
2022-10-04 13:45:45 -0500

    UPDATE

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ port = $PORT\n\
 [theme]\n\
 primaryColor = '#ffe400'\n\
 backgroundColor = '#000000'\n\
-secondaryBackgroundColor = '#ffe400'\n\
+secondaryBackgroundColor = '#a2a2a4'\n\
 textColor = '#ffffff'\n\
 " > ~/.streamlit/config.toml
